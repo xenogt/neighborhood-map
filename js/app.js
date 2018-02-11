2df08d18ea0da89774f37fffcbaf4458dfda5a33
Andy Hsiang <xenogatech@gmail.com>
2018-02-11 15:27:24 -0500

    fixed items from review 2

--- js/app.js
+++ js/app.js
@@ -1,20 +1,28 @@
 var map;
 var markers = [];
-var locations = [
-  {title: 'Tainan Flowers Night Market', location: {lat: 23.0114087, lng: 120.2003643}, weatherapiurl: 'http://api.wunderground.com/api/8b2bf4a9a6f86794/conditions/q/taiwan/Tainan.json'},
-  {title: 'Fengjia Night Market', location: {lat: 24.1759238, lng: 120.6456042},  weatherapiurl: 'http://api.wunderground.com/api/8b2bf4a9a6f86794/conditions/q/taiwan/Taichung.json'},
-  {title: 'Shilin Night Market', location: {lat: 25.0878683, lng: 121.5240774},  weatherapiurl: 'http://api.wunderground.com/api/8b2bf4a9a6f86794/conditions/q/taiwan/Taipei.json'},
-  {title: 'Luodong Night Market', location: {lat: 24.6760296, lng: 121.5240774},  weatherapiurl: 'http://api.wunderground.com/api/8b2bf4a9a6f86794/conditions/q/taiwan/Yilan.json'},
-  {title: 'Liouhe Tourist Night Market', location: {lat: 22.6320758, lng: 120.2991547},  weatherapiurl: 'http://api.wunderground.com/api/8b2bf4a9a6f86794/conditions/q/zmw:00000.1.46740.json'},
-  {title: 'Keelung Miaokou Night Market', location: {lat: 25.1283104, lng: 121.7435851},  weatherapiurl: 'http://api.wunderground.com/api/8b2bf4a9a6f86794/conditions/q/taiwan/Keelung.json'},
-  {title: 'Raohe St. Night Market', location: {lat: 25.0509061, lng: 121.5776476},  weatherapiurl: 'http://api.wunderground.com/api/8b2bf4a9a6f86794/conditions/q/taiwan/Taipei.json'},
-  {title: 'Tonghua Night Market', location: {lat: 25.0302805, lng: 121.5542386},  weatherapiurl: 'http://api.wunderground.com/api/8b2bf4a9a6f86794/conditions/q/taiwan/Taipei.json'},
-  {title: 'Huaxi St. Night Market', location: {lat: 25.0385988, lng: 121.4984426},  weatherapiurl: 'http://api.wunderground.com/api/8b2bf4a9a6f86794/conditions/q/taiwan/Taipei.json'},
-  {title: 'Rueifong Night Market', location: {lat: 22.6662632, lng: 120.2993539},  weatherapiurl: 'http://api.wunderground.com/api/8b2bf4a9a6f86794/conditions/q/zmw:00000.1.46740.json'}
-];
+var locations = [];
+const endpoint = 'locations.json';
 
 function initMap() {
-  ko.applyBindings(new ViewModel());
+  fetch(endpoint)
+  .then(blob => blob.json())
+  .then(data => {
+    for (var key in data) {
+      if (data.hasOwnProperty(key)) {
+        locations.push(data[key]);
+      }
+    }
+    ko.applyBindings(new ViewModel());
+  }).catch((error) => {
+    if(error.message === 'Failed to fetch') {
+      data.forEach(function(location) {
+        locations.push(location);
+      });
+      ko.applyBindings(new ViewModel());
+    } else {
+      alert(error.message);
+    }
+  });
 }
 
 
@@ -53,6 +61,10 @@ var ViewModel = function() {
       }
     });       
   }, self);
+
+  // google.maps.event.addDomListener(window, 'resize', function() {
+  //   map.fitBounds(bounds); // `bounds` is a `LatLngBounds` object
+  // });
 };
 
 
@@ -209,3 +221,9 @@ function setBackground(event) {
   }
 }
 
+
+//handles 
+function errorhandler() {
+  // console.log(this);
+  alert('Google maps failed to load!\ncheck the api url');
+}
---
+++ js/data.js
@@ -0,0 +1,12 @@
+var data = [
+    {title: 'Tainan Flowers Night Market', location: {lat: 23.0114087, lng: 120.2003643}, weatherapiurl: 'http://api.wunderground.com/api/8b2bf4a9a6f86794/conditions/q/taiwan/Tainan.json'},
+    {title: 'Fengjia Night Market', location: {lat: 24.1759238, lng: 120.6456042},  weatherapiurl: 'http://api.wunderground.com/api/8b2bf4a9a6f86794/conditions/q/taiwan/Taichung.json'},
+    {title: 'Shilin Night Market', location: {lat: 25.0878683, lng: 121.5240774},  weatherapiurl: 'http://api.wunderground.com/api/8b2bf4a9a6f86794/conditions/q/taiwan/Taipei.json'},
+    {title: 'Luodong Night Market', location: {lat: 24.6760296, lng: 121.5240774},  weatherapiurl: 'http://api.wunderground.com/api/8b2bf4a9a6f86794/conditions/q/taiwan/Yilan.json'},
+    {title: 'Liouhe Tourist Night Market', location: {lat: 22.6320758, lng: 120.2991547},  weatherapiurl: 'http://api.wunderground.com/api/8b2bf4a9a6f86794/conditions/q/zmw:00000.1.46740.json'},
+    {title: 'Keelung Miaokou Night Market', location: {lat: 25.1283104, lng: 121.7435851},  weatherapiurl: 'http://api.wunderground.com/api/8b2bf4a9a6f86794/conditions/q/taiwan/Keelung.json'},
+    {title: 'Raohe St. Night Market', location: {lat: 25.0509061, lng: 121.5776476},  weatherapiurl: 'http://api.wunderground.com/api/8b2bf4a9a6f86794/conditions/q/taiwan/Taipei.json'},
+    {title: 'Tonghua Night Market', location: {lat: 25.0302805, lng: 121.5542386},  weatherapiurl: 'http://api.wunderground.com/api/8b2bf4a9a6f86794/conditions/q/taiwan/Taipei.json'},
+    {title: 'Huaxi St. Night Market', location: {lat: 25.0385988, lng: 121.4984426},  weatherapiurl: 'http://api.wunderground.com/api/8b2bf4a9a6f86794/conditions/q/taiwan/Taipei.json'},
+    {title: 'Rueifong Night Market', location: {lat: 22.6662632, lng: 120.2993539},  weatherapiurl: 'http://api.wunderground.com/api/8b2bf4a9a6f86794/conditions/q/zmw:00000.1.46740.json'}
+  ];
